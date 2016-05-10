f0cc5a1706c151705d266eaa399e1871f53369f1
damienallen <contact@dallen.co>
2016-05-10 23:53:33 +0200

    Adjusted footer image

--- dallen/settings.py
+++ dallen/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dallen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dallen',
+        'USER': 'db_user',
+        'PASSWORD': 'VpFSpx5CZP79',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
