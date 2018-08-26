c746431e20f3c7e9378e864e79ce4d3be32ba0d7
damienallen <contact@dallen.co>
2018-08-26 17:49:28 +0200

    Added handwriting and more text styling

--- gulpfile.js
+++ gulpfile.js
@@ -46,8 +46,9 @@ function images() {
 function libraries() {
     return merge(
         // Dependencies from node_modules
-        gulp.src('node_modules/jquery/dist/jquery.js').pipe(gulp.dest(jsDir)),
-        gulp.src('node_modules/bootstrap/dist/**/*').pipe(gulp.dest(buildDir)),
+        gulp.src('node_modules/jquery/dist/jquery.min.js').pipe(gulp.dest(jsDir)),
+        gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js').pipe(gulp.dest(jsDir)),
+        gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css').pipe(gulp.dest(cssDir)),
         gulp.src('node_modules/trumbowyg/dist/trumbowyg.min.js').pipe(replace('ui/icons.svg', '../svgs/trumbowyg-icons.svg')).pipe(gulp.dest(jsDir)),
         gulp.src('node_modules/trumbowyg/dist/ui/icons.svg').pipe(rename('trumbowyg-icons.svg')).pipe(gulp.dest(svgDir)),
         gulp.src('node_modules/trumbowyg/dist/ui/trumbowyg.min.css').pipe(gulp.dest(cssDir)),
