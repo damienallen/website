35364a36dce0784fc6ec1d036f4e038b3b08d8fe
Damien Allen <contact@dallen.co>
2020-02-02 14:57:21 +0100

    ES6 javascript

--- src/index.js
+++ src/index.js
@@ -1,84 +1,104 @@
-function sendEmail() {
+// Import stylesheets
+import 'normalize.css'
+import './styles/base.scss'
+import './styles/layout.scss'
 
-    var form_data = {
+const sendEmail = () => {
+
+    const formData = {
         name : $('#id_name').val(),
         email : $('#id_email').val(),
         subject : $('#id_subject').val(),
         message : $('#message-text').val(),
         recaptcha: $('#g-recaptcha-response').val()
-    };
+    }
 
-    console.log(form_data);
+    console.log(formData)
 
     $.ajax({
         url : "/contact/",
         type : "POST",
-        data : form_data,
-        success : function(data) {
+        data : formData,
+        success : (data) => {
             // Show status modal
-            $('#contact-modal-title').text('Message sent!');
-            $('#contact-modal-body').text('I will try to return your message promptly.');
-            $('#contact-modal').modal('show');
+            $('#contact-modal-title').text('Message sent!')
+            $('#contact-modal-body').text('I will try to return your message promptly.')
+            $('#contact-modal').modal('show')
 
             // Disable form
-            $('#submit-button').text('Sent!');
-            $('#submit-button').prop('disabled', true);
+            $('#submit-button').text('Sent!')
+            $('#submit-button').prop('disabled', true)
         },
-        error : function(data) {
+        error : (data) => {
             // Show status modal
-            $('#contact-modal-title').text('Message not sent!');
-            $('#contact-modal-body').text(data.responseJSON.message);
-            $('#contact-modal').modal('show');
+            $('#contact-modal-title').text('Message not sent!')
+            $('#contact-modal-body').text(data.responseJSON.message)
+            $('#contact-modal').modal('show')
         }
-    });
+    })
 
 }
 
+const adjustOpacity = () => {
+    var windowHeight = window.innerHeight
+    var paddingOffset = 10
 
-$(document).ready(function () {
+    var coverOpacity = (windowHeight - paddingOffset - $(this).scrollTop())/windowHeight
+    if (coverOpacity < 0) {
+        coverOpacity = 0
+    }
+
+    var boxShadowInitialOpacity = 0.2
+    var boxShadowOpacity = boxShadowInitialOpacity * coverOpacity
+    var boxShadowValue = '0 3px 15px rgba(0,0,0,' + boxShadowOpacity + ')'
+    $('.cover').css({opacity: coverOpacity})
+    $('#work').css({boxShadow: boxShadowValue})
+}
+
+$(document).ready(() => {
 
     // Get CSRF tokens for AJAX requests
-    function csrfSafeMethod(method) {
+    const csrfSafeMethod = (method) => {
         // these HTTP methods do not require CSRF protection
-        return (/^(HEAD|OPTIONS|TRACE)$/.test(method));
+        return (/^(HEAD|OPTIONS|TRACE)$/.test(method))
     }
     $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
+        beforeSend: (xhr, settings) => {
             if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
+                xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'))
             }
         }
-    });
+    })
 
     // Hide modal initially
-    $('#contact-modal').modal({ show: false});
+    $('#contact-modal').modal({ show: false})
 
     // Set initial background opacity and fade cover background on scroll
-    adjustOpacity();
-    $(window).scroll(function () {
-        adjustOpacity();
-    });
+    adjustOpacity()
+    $(window).scroll(() => {
+        adjustOpacity()
+    })
 
     // Add padding to in-page nav
-    var offset = 50;
+    var offset = 50
 
-    $('#navbar-links #sections a, .down-arrow a').click(function(event) {
-        event.preventDefault();
+    $('#navbar-links #sections a, .down-arrow a').click((event) => {
+        event.preventDefault()
 
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top - offset
-        }, 800);
+        }, 800)
 
-    });
+    })
 
     // Add tooltips
-    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip()
 
     // Initialize contact form
-    $('#contact-form').on('submit', function(event){
-        event.preventDefault();
-        sendEmail();
-    });
+    $('#contact-form').on('submit', (event) => {
+        event.preventDefault()
+        sendEmail()
+    })
 
     // Add WYSIWYG editor to textarea
     $('#message-text').trumbowyg({
@@ -92,22 +112,9 @@ $(document).ready(function () {
             ['unorderedList', 'orderedList'],
             ['fullscreen']
         ]
-    });
+    })
 
-});
+    const now = new Date()
+    console.log(`Loaded at ${now.toLocaleTimeString()} on ${now.toLocaleDateString()}`)
 
-function adjustOpacity() {
-    var windowHeight = window.innerHeight;
-    var paddingOffset = 10;
-
-    var coverOpacity = (windowHeight - paddingOffset - $(this).scrollTop())/windowHeight;
-    if (coverOpacity < 0) {
-        coverOpacity = 0;
-    }
-
-    var boxShadowInitialOpacity = 0.2;
-    var boxShadowOpacity = boxShadowInitialOpacity * coverOpacity;
-    var boxShadowValue = '0 3px 15px rgba(0,0,0,' + boxShadowOpacity + ')';
-    $('.cover').css({opacity: coverOpacity})
-    $('#work').css({boxShadow: boxShadowValue})
-}
+})
