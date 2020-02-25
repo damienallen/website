289232ac107e3073aacc391446026390502241cf
Damien Allen <damien@mx3d.com>
2020-02-26 00:34:05 +0100

    Basic sendgrid setup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from werkzeug.middleware.proxy_fix import ProxyFix
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import os
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -12,4 +15,30 @@ def hello():
 
 @app.route("/api/submit", methods=["POST"])
 def submit():
+
+    # Extract request data
+    # name = request.form.get("name")
+    from_email = request.form.get("email")
+    subject = request.form.get("subject")
+    message = request.form.get("message")
+
+    to_email = os.environ.get("CONTACT_EMAIL")
+
+    # Send email
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_email,
+        subject=f"[Contact Form] {subject}",
+        html_content=message,
+    )
+
+    try:
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_KEY"))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(str(e))
+
     return jsonify(request=request.form)
