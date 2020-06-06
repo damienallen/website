ffe411cabd601fdcfbcd6b9fc7cc86b66fa45f2f
Damien Allen <contact@dallen.co>
2020-06-06 17:22:06 +0000

    Functional email form

--- app/main.py
+++ app/main.py
@@ -1,42 +1,45 @@
 from flask import Flask, request, jsonify
 from werkzeug.middleware.proxy_fix import ProxyFix
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, From
+import logging
 import os
 
 from form import ContactForm
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+app.logger.setLevel(logging.DEBUG)
 
 
-@app.route("/api/")
+@app.route("/api")
 def hello():
-    return f"Why, hello there kind stranger."
+    return "Why, hello there kind stranger."
 
 
-@app.route("/api/submit/", methods=["POST"])
+@app.route("/api/submit", methods=["POST"])
 def submit():
 
+    # Run validation
     contact_form = ContactForm(request.form)
-
     is_valid = contact_form.validate()
     status_code = 200 if is_valid else 400
-    validation_errors = [str(e) for e in contact_form.errors.items()]
+    errors = [str(e) for e in contact_form.errors.items()]
 
     if request.method == "POST" and is_valid:
 
         # Extract request data
-        # name = request.form.get("name")
+        name = request.form.get("name")
         from_email = request.form.get("email")
         subject = request.form.get("subject")
         message = request.form.get("message")
 
+        # Get contact email from environment
         to_email = os.environ.get("CONTACT_EMAIL")
 
-        # Send email
+        # Build sendgrid mail object
         message = Mail(
-            from_email=from_email,
+            from_email=From(from_email, name),
             to_emails=to_email,
             subject=f"[Contact Form] {subject}",
             html_content=message,
@@ -45,10 +48,13 @@ def submit():
         try:
             sg = SendGridAPIClient(os.environ.get("SENDGRID_KEY"))
             response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(str(e))
+            
+            if response.status_code == 202:
+                app.logger.debug("Message sent sucessfully.")
 
-    return jsonify(request=request.form, errors=validation_errors), status_code
+        except Exception as e:
+            app.logger.error(e)
+            errors.append("Failed to connect to sendgrid.")
+            status_code = 500
+
+    return jsonify(request=request.form, errors=errors), status_code
