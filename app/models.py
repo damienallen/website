986b4517cc1540310d614d3b64df1580876d1951
Damien Allen <dev@dallen.co>
2025-01-12 21:02:20 +0100

    Remove node, docker and python

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from wtforms import Form, StringField, validators
-
-
-class ContactForm(Form):
-    name = StringField("Name", [validators.Length(min=1, max=30)])
-    email = StringField("Email", [validators.Length(min=5, max=30), validators.Email()])
-    subject = StringField("Subject", [validators.Length(min=1, max=100)])
-    message = StringField("Message", [validators.Length(min=5, max=2000)])
-    check = StringField("Check")
